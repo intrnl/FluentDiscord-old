7ddb58a8ec5fa1f3972666245ebf03a3f83eb88f
intrnl <intrnl28@gmail.com>
2018-07-02 15:12:27 +0700

    use bdv2 config file

--- bdv1_build.js
+++ bdv1_build.js
@@ -2,12 +2,26 @@ const fs = require('fs')
 const sass = require('sass')
 const cleancss = require('clean-css')
 
-const meta = `/*//META${JSON.stringify(JSON.parse(fs.readFileSync('./FluentDiscord/_meta.json')))}*//**/\r\n`
+// Make a META configuration using BDv2's config.json file
+console.log('Reading configuration')
+const config = JSON.parse(fs.readFileSync('./FluentDiscord/config.json'))['info']
 
+const meta = {
+  name: config.name,
+  description: config.description,
+  author: config.authors.map(author => author.name).join(', '),
+  version: config.version.toString(),
+  source: config.source
+}
+
+const builtmeta = `/*//META${JSON.stringify(meta)}*//**/\r\n`
+
+// Build the theme file and minify it.
 console.log('Building theme file')
 const built = sass.renderSync({ file: './FluentDiscord/bdv1.scss' })
 const mini = new cleancss({ level: 2 }).minify(built.css)
 
+// Write to disk.
 console.log('Writing to disk')
-fs.writeFileSync('./bdv1/FluentDiscord.theme.css', meta + built.css)
-fs.writeFileSync('./bdv1/FluentDiscord.min.theme.css', meta + mini.styles)
+fs.writeFileSync('./bdv1/FluentDiscord.theme.css', builtmeta + built.css)
+fs.writeFileSync('./bdv1/FluentDiscord.min.theme.css', builtmeta + mini.styles)
