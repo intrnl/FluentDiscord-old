d05ce51ec23a47064fbb0fe39d4db3932e3d04a8
intrnl <intrnl28@gmail.com>
2018-07-21 18:33:02 +0700

    remove crass

--- bdv1_build.js
+++ bdv1_build.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const sass = require('sass')
 const cleancss = require('clean-css')
-const crass = require('crass')
 
 // Make a META configuration using BDv2's config.json file
 console.log('Reading configuration')
@@ -22,11 +21,9 @@ console.log('Building theme file')
 const built = sass.renderSync({ file: './FluentDiscord/bdv1.scss' })
 const remote = sass.renderSync({ file: './FluentDiscord/bdv1_remote.scss' })
 const miniclean = new cleancss({ level: 2 }).minify(built.css)
-const minicrass = crass.parse(built.css).optimize({ o1: true })
 
 // Write to disk.
 console.log('Writing to disk')
 fs.writeFileSync('./bdv1/FluentDiscord.theme.css', builtmeta + built.css)
 fs.writeFileSync('./bdv1/FluentDiscord.min.theme.css', builtmeta + miniclean.styles)
-fs.writeFileSync('./bdv1/FluentDiscord.crass.theme.css', builtmeta + minicrass.toString())
 fs.writeFileSync('./bdv1/FluentDiscord.remote.theme.css', builtmeta + remote.css)
