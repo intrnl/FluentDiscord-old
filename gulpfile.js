b8c41b8bae498c75d08fd2e28b18074fcb320bae
intrnl <intrnl28@gmail.com>
2018-08-21 20:03:42 +0700

    gulp n stuff

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+let pump = require('pump')
+let gulp = require('gulp')
+
+let rename = require('gulp-rename')
+let sass = require('gulp-sass')
+let prefix = require('gulp-autoprefixer')
+let cssnano = require('gulp-cssnano')
+let sourcemap = require('gulp-sourcemaps')
+let { prepend } = require('gulp-inject-string')
+
+
+let { info: config } = require('./FluentDiscord/config.json')
+
+let meta = {
+  name: config.name,
+  description: config.description,
+  author: config.authors.map(author => author.name).join(', '),
+  version: config.version.toString(),
+  source: config.source
+}
+
+let builtmeta = `/*//META${JSON.stringify(meta)}*//**/\r\n`
+
+
+gulp.task('build', function () {
+  return pump([
+    gulp.src('./FluentDiscord/bdv1.scss'),
+    sourcemap.init(),
+    sass().on('error', sass.logError),
+    prefix(),
+    prepend(builtmeta),
+    rename(`${config.name}.theme.css`),
+    sourcemap.write('.'),
+    gulp.dest('./bdv1'),
+    cssnano(),
+    prepend(builtmeta),
+    rename(`${config.name}.min.theme.css`),
+    gulp.dest('./bdv1')
+  ])
+})
+
+gulp.task('watch', function () {
+  gulp.watch('./FluentDiscord/**/*.scss', ['build'])
+})
